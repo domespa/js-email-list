cdbb282c2cf2e3eebca3ff3890dfb1d84a16d744
domespa <dumiii1988@gmail.com>
2025-01-08 16:42:49 +0100

    feat: add function for update the email + button for refresh

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,16 @@
 // FUNZIONE PER GENERARE 10 EMAILS RANDOM
+const emailList = [];
 function createEmail() {
-    const emailList = [];
-  //  console.log(emailList) //
+    emailList.length = 0;
 
     for (let i = 0; i < 10; i++){
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then((res) => {
-            emailList.push(res.data.response);         
+            emailList.push(res.data.response);
+            if (emailList.length === 10) {
+                updatedEmails();
+                console.log(emailList);
+            }
         })
         .catch(error => {
             console.error('Si è verificato un errore:', error);
@@ -15,4 +19,9 @@ function createEmail() {
 
 
 
-
+// FUNZIONE PER INVIARE LE EMAIL ALL'HTML
+function updatedEmails() {
+    for (let i = 0; i < 10; i++) {
+        document.getElementById(`email${i + 1}`).innerHTML = emailList[i];
+    }
+};
---
+++ js/script.js
@@ -0,0 +1,8 @@
+//createEmail();
+// console.log(emailList)
+   
+// DOM ELM
+const btnRefreshElm = document.getElementById("refresh")
+
+// EVENTS
+btnRefreshElm.addEventListener("click", createEmail)
